008420bc03ecb375abe7d52f302a791262d1f152
eralogos <zhuoperation@gmail.com>
2018-12-13 00:42:20 -0500

    track tutorial

---
+++ resources/views/pages/tutorial.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-2">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <h3 class="card-header">Tutorial</h3>
+                <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <img src="images/admintoken.png" class="img-fluid">
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                </div>
+                </div>
+                images/admintoken.png
+                Sample url: https://i.imgur.com/1XgBtTH.jpg
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
