8a80f5ad12afbf2f34bfd33755ec18632bd88f33
eralogos <zhuoperation@gmail.com>
2019-05-08 20:35:35 -0400

    html icon and welcome page update

--- resources/views/pages/info.blade.php
+++ resources/views/pages/info.blade.php
@@ -8,21 +8,27 @@
                 <h2 class="card-header">Welcome to Graffiti</h2>
 
                 <div class="card-body">
-                    <p>Graffiti is a Volunteer Geographic Information interface 
+                    <div>Graffiti is a Volunteer Geographic Information interface 
                         for location-based communication, which is the outcome of my Honours Thesis
                         "The Design of a Location-based Neighborhood Game 
-                        that Stimulates the Generation of Social Capital". 
-                        The base game is a location-based social medium that generates social captial
+                        that Stimulates the Generation of Social Capital" 
+                        (<a href="https://github.com/eralogos/Graffiti-open-data-archive">https://github.com/eralogos/Graffiti-open-data-archive"</a>). 
+                        <br />
+                        The base game named <b>COMM</b> is a location-based social medium that generates social captial
                          through local online communication. 
                          Some game mechanics are applied to motivate participations using Self-Determination Theory. 
-                          The interface was renamed as <b>COMM</b> in a recent update. 
+                         <br />
                          <b>Station</b> is a hosted interface for creating and sharing "transit routes" 
                           that link similar places of interest together in Waterloo Region.
-                    </p>
+                    </div>
                     <a href="/tutorial">Go to Tutorial for more information</a><br />
-                    Click "Login" or "Register" to get access to your game account<br /> 
+                    Click "Login" or "Register" to get access to your game account<br />
+                    <br />
+                    Important notice: 
+                    <br />
                     USE FRESH, NEVER-USED PASSWORD FOR THE GAME ACCOUNT<br />
                     IT IS HIGH RISK TO USE THE SAME PASSWORD AS THE PASSWORD USED IN YOUR OTHER ACCOUNTS!<br />
+                    Toxic and offensive posts will be deleted by admin<br /> 
                 </div>
             </div>
         </div>
